ecc41f6c2dcbf928cab4c72d2bad0cb2a5e2ea39
StephenFireball <stepanveremeev@gmail.com>
2022-05-22 01:33:04 +0300

    notification's sound + gradle upgrade

--- app/src/main/java/com/samsung/mytime/Notifications.java
+++ app/src/main/java/com/samsung/mytime/Notifications.java
@@ -6,6 +6,8 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -15,11 +17,13 @@ public class Notifications extends BroadcastReceiver {
     public static final String channelID = "myTimeNotifications";
     @Override
     public void onReceive(Context context, Intent intent) {
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         android.app.Notification notification = new NotificationCompat.Builder(context, channelID)
                 .setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
                 .setContentTitle("My Time")
                 .setContentText("Name: " + EventEditActivity.eventName + "\nTime: " + EventEditActivity.time.toString())
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setSound(uri)
                 .setAutoCancel(true)
                 .build();
 
